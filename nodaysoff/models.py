9d32babc124ea163f8759b71f892cf232c0bfb99
attila5287 <atiturkoz@hotmail.com>
2020-01-13 08:50:39 -0700

    proday single form with five initial fields

--- nodaysoff/models.py
+++ nodaysoff/models.py
@@ -16,6 +16,8 @@ class Proday(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
--- nodaysoff/prodays/forms.py
+++ nodaysoff/prodays/forms.py
@@ -7,6 +7,7 @@ class ProDayForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
- 
- 
- 
+
+# class Activity(FlaskForm):
+#     pass
+65
