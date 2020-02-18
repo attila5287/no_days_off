78a08d94a5afc5c4b662acbeebff231ac7021884
attila5287 <atiturkoz@hotmail.com>
2020-02-18 04:26:46 -0700

    fx heroku

--- nodaysoff/users/routes.py
+++ nodaysoff/users/routes.py
@@ -42,8 +42,31 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
+
+
 @users.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if user!=True and form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        print('test user obj attributes')
+        print(user.password)
+        print('test req form pw')
+        print(request.form["password"])
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+
+@users.route("/log1n", methods=['GET', 'POST'])
+def log1n():
     pass
     if current_user.is_authenticated:
         return redirect(url_for('main.home'))
@@ -57,7 +80,7 @@ def login():
         print(user.password)
         print('test req form pw')
         print(request.form["password"])
-        if user and bcrypt.check_password_hash(user.password, request.form["password"]):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
